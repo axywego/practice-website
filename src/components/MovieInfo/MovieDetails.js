cd4e6169c6161d92369f131b7ad95cfe6f513e32
axywego <pukeotim@gmail.com>
2024-07-11 15:09:41 +0300

    fixed bugs w/ favourites

--- src/components/MovieInfo/MovieDetails.js
+++ src/components/MovieInfo/MovieDetails.js
@@ -4,20 +4,14 @@ import StarIcon from "./star.png"
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w342';
 
 const MovieDetails = (props) => {
-    const [movies, setMovies] = useState(props.movies);
-    const [movie, setMovie] = useState(props.currentMovie);
-    const [index, setIndex] = useState(props.index);
-    const [isFavourite, setIsFavourite] = useState(props.favourite.some(item => item.id === movie.id));
 
-    const [poster, setPoster] = useState(`${imageBaseUrl}${movie.poster_path}`);
-    const [back, setBack] = useState(`${imageBaseUrl}${movie.backdrop_path}`);
-    const [title, setTitle] = useState(movie.title);
-    const [score, setScore] = useState(movie.vote_average);
-    const [rating, setRating] = useState(movie.popularity);
-    const [releaseDate, setReleaseDate] = useState((new Date(movie.release_date))
-                                          .toLocaleString("en-US", 
-                                          { month: 'long', day: 'numeric', year: 'numeric' }));
-    const [description, setDescription] = useState(movie.overview);
+    const movies = props.movies;
+    const favourite = props.favourite;
+    const [currentMovie, setCurrentMovie] = useState(props.currentMovie);
+    const [index, setIndex] = useState(props.index);
+
+    const [isFavourite, setIsFavourite] = useState(favourite.some(item => (
+                                        item.id === currentMovie.id)));
 
     const [isMobile, setIsMobile] = useState(false);
 
@@ -36,40 +30,34 @@ const MovieDetails = (props) => {
 
     const handlerNextMovie = () => {
         if(index != 19){
+            setIsFavourite(favourite.some(item => (
+                item.id === movies[index + 1].id)));
+            setCurrentMovie(movies[index + 1]);
             setIndex(index + 1);
-            setMovie(movies[index + 1]);
-            setTitle(movies[index + 1].title);
-            setScore(movies[index + 1].vote_average);
-            setRating(movies[index + 1].popularity);
-            setReleaseDate((new Date(movies[index + 1].release_date))
-                            .toLocaleString("en-US", 
-                            { month: 'long', day: 'numeric', year: 'numeric' }));
-            setDescription(movies[index + 1].overview);
-            setBack(`${imageBaseUrl}${movies[index + 1].backdrop_path}`);
-            setPoster(`${imageBaseUrl}${movies[index + 1].poster_path}`);
-            setIsFavourite(props.favourite.includes(movies[index + 1]));
         }
     }
 
     const favouriteHandler = () => {
         if(isFavourite === true) {
             setIsFavourite(false);
-            props.onGetIsFavourite(false, movie);
+            props.onGetIsFavourite(false, currentMovie);
         }
         else if(isFavourite === false) {
             setIsFavourite(true);
-            props.onGetIsFavourite(true, movie);
+            props.onGetIsFavourite(true, currentMovie);
         }
     }
 
     return (
         <div className="details">
-            <img className="details__back" src={back} alt="poster is missing"></img>
+            <img className="details__back" 
+                 src={`${imageBaseUrl}${currentMovie.backdrop_path}`} 
+                 alt="poster is missing"/>
             <div className="details__btns">
-                <button onClick={() => props.onIsActive(false, movie)}>
+                <button onClick={() => props.onIsActive(false, currentMovie)}>
                    {"<"} Back to List
                 </button>
-                {index !== 19 && (
+                {index != 19 && (
                     <button onClick={handlerNextMovie}>
                         Next Movie {">"}
                     </button>
@@ -77,10 +65,11 @@ const MovieDetails = (props) => {
                 }
             </div>
             <div className="details__info">
-                <img className="details__info-poster" src={poster}></img>
-                <div className="details__info-text">
+                <img className="details__info-poster"
+                     src={`${imageBaseUrl}${currentMovie.poster_path}`}/>
+                <div className="details__info-text">    
                     <div className="title-btn">
-                        <h2>{title}</h2>
+                        <h2>{currentMovie.title}</h2>
                         <button className="details__info__favorite-button"
                             onClick={favouriteHandler}>
                             {isMobile ? 
@@ -94,8 +83,10 @@ const MovieDetails = (props) => {
                         </button>
                     </div>
                     <div className="description">
-                        <h4>Score: {score} | Rating: {rating} | Release Date: {releaseDate}</h4>
-                        <p>{description}</p>
+                        <h4>Score: {currentMovie.vote_average} 
+                            | Rating: {currentMovie.popularity} 
+                            | Release Date: {currentMovie.release_date}</h4>
+                        <p>{currentMovie.overview}</p>
                     </div>   
                 </div>
             </div>
